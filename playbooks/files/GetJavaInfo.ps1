f38073c2d2988ea87ba249c95c1aa2cbf8f5c72d
lomk <materynko@gmail.com>
2017-01-10 12:37:39 +0200

    2

--- playbooks/files/GetJavaInfo.ps1
+++ playbooks/files/GetJavaInfo.ps1
@@ -50,19 +50,19 @@ PROCESS {
             if($AppDisplayName -like '*Java*' -And $AppPublisher -like 'Oracle*')
             { 
                 $OutputObj = New-Object -TypeName PSobject
-                $OutputObj | Add-Member -MemberType NoteProperty -Name ComputerName -Value $Computer.ToUpper()
+                #$OutputObj | Add-Member -MemberType NoteProperty -Name ComputerName -Value $Computer.ToUpper()
                 $OutputObj | Add-Member -MemberType NoteProperty -Name AppName -Value $AppDisplayName
                 $OutputObj | Add-Member -MemberType NoteProperty -Name AppVersion -Value $AppVersion
                 $OutputObj | Add-Member -MemberType NoteProperty -Name AppVendor -Value $AppPublisher
                 $OutputObj | Add-Member -MemberType NoteProperty -Name InstalledDate -Value $AppInstalledDate
                 $OutputObj | Add-Member -MemberType NoteProperty -Name UninstallKey -Value $AppUninstall
-                $OutputObj | Add-Member -MemberType NoteProperty -Name AppGUID -Value $AppGUID
-                if ($RegistryView -eq 'Registry32')
-                {
-                    $OutputObj | Add-Member -MemberType NoteProperty -Name Arch -Value '32'
-                } else {
-                    $OutputObj | Add-Member -MemberType NoteProperty -Name Arch -Value '64'
-                }
+                #$OutputObj | Add-Member -MemberType NoteProperty -Name AppGUID -Value $AppGUID
+                #if ($RegistryView -eq 'Registry32')
+                #{
+                 #   $OutputObj | Add-Member -MemberType NoteProperty -Name Arch -Value '32'
+                #} else {
+                 #   $OutputObj | Add-Member -MemberType NoteProperty -Name Arch -Value '64'
+                #}
                 $OutputObj
             }
             # Export to a file
